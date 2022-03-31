a4f8f78ddc11f05aa7dda6fbacfc948112e32a58
Nikita Venediktov <nikben2105@gmail.com>
2022-04-01 00:04:43 +0300

    added start KIS model, air_cab.db, login and registration functions

---
+++ include/server.h
@@ -0,0 +1,19 @@
+#ifndef server_h
+#define server_h
+
+#define _CRT_SECURE_NO_WARNINGS
+#define TRUE 1
+#define FALSE 0
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "sqlite3.h" 
+#include "string.h"
+
+short access; 
+
+void Registraion(sqlite3* db);
+void Login(sqlite3* db);
+short executeSQL(sqlite3* db, const char* sql, int(*callback)(void*, int, char**, char**), void* data, short success_inform);
+
+#endif
